b360ce34ead5884186342b4e086bb3a7a078ad5f
ManuelOrtizG <manuel.a.ortiz-garcia@capgemini.com>
2021-10-26 18:51:46 +0200

    1 file modified

--- src/app/mars/mars.module.ts
+++ src/app/mars/mars.module.ts
@@ -3,12 +3,13 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { MarsComponent } from './mars.component';
 import { PhotoDataService } from '../rober-photos.service';
+import { MarsRoutingModule } from './mars-routing.module';
 
 //primeng
 import { DataViewModule } from 'primeng/dataview';
 
 @NgModule({
-  imports: [HttpClientModule, DataViewModule],
+  imports: [HttpClientModule, DataViewModule, MarsRoutingModule],
   declarations: [MarsComponent],
   providers: [HttpClient, PhotoDataService],
 })
